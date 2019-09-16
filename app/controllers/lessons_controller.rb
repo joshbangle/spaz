351116389b91401eae0b319d01259e76d359fffd
Joshua Bangle <joshua.bangle@gmail.com>
2019-09-16 21:42:49 +0000

    permissions for viewing lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,8 @@
 class LessonsController < ApplicationController
-
+  before_action :authenticate_user!
+  before_action :req_enrolled_for_lessons
   def show
+
   end
 
 
@@ -8,6 +10,12 @@ class LessonsController < ApplicationController
 
 
   private
+
+  def req_enrolled_for_lessons
+    if current_user.enrolled_in?(current_lesson.section.guide) != true
+      redirect_to guide_path(current_lesson.section.guide), alert: "You must enroll to view this guide's lessons!"
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
