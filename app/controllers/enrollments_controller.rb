13322efc9a05454217e67575eb56aeab64a23898
Joshua Bangle <joshua.bangle@gmail.com>
2019-09-17 21:11:02 +0000

    added payments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,8 +2,30 @@ class EnrollmentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    if current_guide.premium?
+
+          # Amount in cents
+      @amount = (current_guide.cost * 100).to_i
+
+      customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: params[:stripeToken]
+      )
+
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: @amount,
+        description: 'Spaz Guide Payment',
+        currency: 'usd'
+      )
+    end
+    
     current_user.enrollments.create(guide: current_guide)
     redirect_to guide_path(current_guide)
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to root_path
   end
 
   private
