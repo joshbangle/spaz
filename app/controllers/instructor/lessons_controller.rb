17fec9383166f3540fd35a6300fee8b6bc12c76f
Joshua Bangle <joshua.bangle@gmail.com>
2019-09-18 05:02:31 +0000

    added drag and drop lesson ordering

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section
+  before_action :require_authorized_for_current_section, only: [:new, :create]
+  before_action :require_auth_for_current_lesson, only: [:update]
 
   def new
     @lesson = Lesson.new
@@ -11,10 +12,25 @@ class Instructor::LessonsController < ApplicationController
     redirect_to instructor_guide_path(current_section.guide)
   end
 
+  def update
+    current_lesson.update_attributes(lesson_params)
+    render plain: 'updated!'
+  end
+
   private
+
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
 
   def require_authorized_for_current_section
     if current_section.guide.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+  def require_auth_for_current_lesson
+    if current_lesson.section.guide.user != current_user
       return render plain: 'Unauthorized', status: :unauthorized
     end
   end
@@ -25,6 +41,6 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle, :video)
+    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
   end
 end
